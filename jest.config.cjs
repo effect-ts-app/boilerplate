02cc7b3d0462db6aeff5960419c3edb3ed69e363
Patrick Roza <contact@patrickroza.com>
2022-11-27 14:07:28 +0100

    cleanup

--- jest.config.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  cacheDirectory: "./.jest-cache",
-  projects: ["<rootDir>/apps/api", "<rootDir>/apps/frontend-nuxt", "<rootDir>/packages/*"].concat(!process.env.GITHUB_ACTIONS ? ["<rootDir>/apps/api/_test"] : [])
-}
--- jest.polyfill.js
+++
@@ -1,9 +0,0 @@
-require("core-js/stable")
-require("regenerator-runtime/runtime")
-
-const COMPILED = !!process.env.TEST_COMPILED
-
-if (!COMPILED) {
-  require("@effect-ts-app/core/fluent/polyfill/node")
-}
-//require("@effect-ts/system/Tracing/Enable")
