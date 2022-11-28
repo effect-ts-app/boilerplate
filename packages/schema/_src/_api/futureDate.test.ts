71385e3e6bc9ffb7846fef564adb9ff8ad3a7d92
Patrick Roza <contact@patrickroza.com>
2022-11-28 13:03:18 +0100

    cleanup

--- packages/schema/_src/_api/futureDate.test.ts
+++ packages/schema/_src/_api/futureDate.test.ts
@@ -1,25 +1,24 @@
-import { describe, expect, it } from "@jest/globals"
-
 import { Constructor, Parser, unsafe } from "../vendor.js"
 import { FutureDate } from "./futureDate.js"
+import { describe, expect, test } from "vitest"
 
 const makeFutureDateUnsafe = Constructor.for(FutureDate) >= unsafe
 const parseFutureDateUnsafe = Parser.for(FutureDate) >= unsafe
 
 describe("Constructor", () => {
-  it("allows a future date", () => {
+  test("allows a future date", () => {
     makeFutureDateUnsafe(new Date(2040, 1, 1))
   })
-  it("disallows a past date", () => {
+  test("disallows a past date", () => {
     expect(() => makeFutureDateUnsafe(new Date(1985, 1, 1))).toThrow()
   })
 })
 
 describe("Parser", () => {
-  it("allows a future date", () => {
+  test("allows a future date", () => {
     parseFutureDateUnsafe(new Date(2040, 1, 1).toISOString())
   })
-  it("disallows a past date", () => {
+  test("disallows a past date", () => {
     expect(() => parseFutureDateUnsafe(new Date(1985, 1, 1).toISOString()))
   })
 })
