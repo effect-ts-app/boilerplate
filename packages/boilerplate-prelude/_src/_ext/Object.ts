ce45d8fa477ed53a05aa1f5e38add59b44aa4c3e
Patrick Roza <contact@patrickroza.com>
2022-11-28 12:05:25 +0100

    add missing types

--- packages/boilerplate-prelude/_src/_ext/Object.ts
+++ packages/boilerplate-prelude/_src/_ext/Object.ts
@@ -1,34 +1,4 @@
 declare global {
-  /**
-   * @tsplus type Date
-   */
-  interface Date {}
-
-  // /**
-  //  * @tsplus type Record
-  //  */
-  // interface Record<K, V> {}
-
-  /**
-   * @tsplus type Object
-   */
-  interface Object {}
-
-  /**
-   * @tsplus type Generator
-   */
-  interface Generator<T = unknown, TReturn = any, TNext = unknown> {}
-
-  /**
-   * @tsplus type Iterator
-   */
-  interface Iterator<T, TReturn = any, TNext = undefined> {}
-
-  /**
-   * @tsplus type function
-   */
-  interface Function {}
-
   interface String {
     /**
      * Split a string into substrings using the specified separator and return them as an array.
