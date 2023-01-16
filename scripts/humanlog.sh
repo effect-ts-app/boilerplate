f043d1345fc61b49f24af2d400cda28a1981763e
Patrick Roza <contact@patrickroza.com>
2023-01-16 13:08:41 +0100

    chore: workaround humanlog crash.

--- scripts/humanlog.sh
+++ scripts/humanlog.sh
@@ -1,7 +1,7 @@
 if which humanlog >/dev/null; then
     # For some reason --skip-unchanged is enabled by default and can't figure out how to disable!
     # https://github.com/humanlogio/humanlog/issues/40
-    humanlog --keep requestId --keep requestName
+    humanlog --keep requestId --keep requestName || (echo 'humanlog crashed!' && cat)
 else
     echo "!! It's recommended to install humanlog, to make these logs more readable. !!"
     while read x ; do echo $x ; done
