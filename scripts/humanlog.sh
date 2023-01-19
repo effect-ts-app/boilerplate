2ce64853b3a737abcbe359e1d21e3fabbcc87db7
Patrick Roza <contact@patrickroza.com>
2023-01-19 09:49:20 +0100

    chore: update humanlog.sh

--- scripts/humanlog.sh
+++ scripts/humanlog.sh
@@ -1,7 +1,7 @@
 if which humanlog >/dev/null; then
     # For some reason --skip-unchanged is enabled by default and can't figure out how to disable!
     # https://github.com/humanlogio/humanlog/issues/40
-    tee dev.log | humanlog --keep requestId --keep requestName --truncate-length 16384 || (echo 'humanlog crashed!' && cat)
+    tee dev.log | (humanlog --keep requestId --keep requestName --truncate-length 16384 || (echo 'humanlog crashed!' && cat))
 else
     echo "!! It's recommended to install humanlog, to make these logs more readable. !!"
     # while read x ; do echo $x ; done
