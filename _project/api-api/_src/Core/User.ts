1c619a93ff0123bee80ab4f58254cf3440c3edc2
Patrick Roza <contact@patrickroza.com>
2023-02-06 12:43:49 +0100

    restructure: api -> main, api-api -> api (#13)

--- _project/api-api/_src/Core/User.ts
+++
@@ -1,5 +0,0 @@
-import type { User } from "@effect-app-boilerplate/models/User"
-import { makeAllDSL, makeOneDSL } from "@effect-app/infra/services/Repository"
-
-export const Users$ = makeAllDSL<User, never>()
-export const User$ = makeOneDSL<User, never>()
--- _project/api/_src/_global.ts
+++ _project/api/_src/_global.ts
@@ -1,3 +1,10 @@
 /* eslint-disable unused-imports/no-unused-imports */
 
-import "@effect-app-boilerplate/api-api/_global"
+import "@effect-app-boilerplate/messages/_global"
+
+/**
+ * @tsplus global
+ */
+import { handle, matchFor } from "@/lib/routing.js"
+
+import "./_global.ext.js"
