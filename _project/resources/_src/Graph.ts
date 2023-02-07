cd6eec05a00ed5777838c1c6fe361a51d0b726a8
Patrick Roza <contact@patrickroza.com>
2023-02-07 13:36:35 +0100

    Fix wrong file

--- _project/resources/_src/Graph.ts
+++
@@ -1,5 +0,0 @@
-// codegen:start {preset: barrel, include: ./Graph/*.ts, exclude: ./Graph/utils.ts, export: { as: 'PascalCase' }, nodir: false }
-export * as Mutation from "./Graph/Mutation.js"
-export * as Query from "./Graph/Query.js"
-
-// codegen:end
--- _project/resources/_src/index.ts
+++ _project/resources/_src/index.ts
@@ -4,7 +4,6 @@ export { ClientEvents } from "./Events.js"
 
 // codegen:start {preset: barrel, include: ./*.ts, exclude: [./_global*.ts, ./index.ts, ./lib.ts, ./Views.ts, ./errors.ts], export: { as: 'PascalCase', postfix: 'Rsc' }}
 export * as EventsRsc from "./Events.js"
-export * as GraphRsc from "./Graph.js"
 export * as HelloWorldRsc from "./HelloWorld.js"
 export * as MeRsc from "./Me.js"
 export * as OperationsRsc from "./Operations.js"
