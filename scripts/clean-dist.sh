87f779842685910438b9479a81d250ebc174df59
Patrick Roza <contact@patrickroza.com>
2023-02-07 09:51:05 +0100

    update scripts

--- scripts/clean-dist.sh
+++ scripts/clean-dist.sh
@@ -31,3 +31,36 @@ do
     rm -f $raw.js $raw.js.map $raw.d.ts $raw.d.ts.map
   fi
 done
+
+
+for d in `find _test/dist -type d | grep -v dist$`
+do
+  src_d="_test/${d#_test/dist/}"
+  if [ ! -d "$src_d" ]; then
+    echo "Removing $d"
+    rm -rf $d
+  fi
+done
+
+for f in `find _test/dist -type f | grep \\\.mjs$`
+do
+  src_f="_test/${f#_test/dist/}"
+  src_f="${src_f%.mjs}.mts"
+  raw="${f%.mjs}"
+  if [ ! -f "$src_f" ]; then
+    echo "Removing $raw.mjs"
+    rm -f $raw.mjs $raw.mjs.map $raw.d.mts $raw.d.mts.map
+  fi
+done
+
+
+for f in `find _test/dist -type f | grep \\\.js$`
+do
+  src_f="_test/${f#_test/dist/}"
+  src_f="${src_f%.js}.ts"
+  raw="${f%.js}"
+  if [ ! -f "$src_f" ]; then
+    echo "Removing $raw.js"
+    rm -f $raw.js $raw.js.map $raw.d.ts $raw.d.ts.map
+  fi
+done
