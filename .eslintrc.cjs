2cd10511bb48c8a8cfb613d4412ea677f7644043
Patrick Roza <contact@patrickroza.com>
2023-02-26 14:50:25 +0100

    update lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
-const makeBase = require("../../.eslintrc.base")
-const base = makeBase(__dirname, true)
-
+const makeBase = require("./.eslintrc.base")
+const base = makeBase(__dirname, true, "tsconfig.all2.json")
 module.exports = {
+  root: true,
   ...base,
+  plugins: base.plugins.concat(["formatjs"]),
   rules: {
     ...base.rules,
     'codegen/codegen': ['error', { presets: require('@effect-app/eslint-codegen-model') }],
     "@typescript-eslint/no-empty-interface": "off"
-  }
+  },
 }
