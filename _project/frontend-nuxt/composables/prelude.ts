0f515cd707ec7c59449204eaf442b5b8c0aeaa4e
Patrick Roza <contact@patrickroza.com>
2023-03-04 16:20:16 +0100

    cleanup fp-ts

--- _project/frontend-nuxt/composables/prelude.ts
+++ _project/frontend-nuxt/composables/prelude.ts
@@ -2,7 +2,7 @@ import * as MO from "@effect-app/prelude/schema"
 import * as These from "@effect-app/schema/custom/These"
 import * as Effect from "@effect/io/Effect"
 import * as Layer from "@effect/io/Layer"
-import * as Either from "@fp-ts/core/Either"
+import * as Either from "@effect/data/Either"
 import * as Option from "@effect-app/core/Option"
 import * as Tag from "@effect/data/Context"
 import * as Order from "@effect-app/core/Order"
@@ -11,7 +11,7 @@ import * as ROArray from "@effect-app/core/Array"
 import type {
   NonEmptyArray,
   NonEmptyReadonlyArray,
-} from "@fp-ts/core/ReadonlyArray"
+} from "@effect/data/ReadonlyArray"
 
 export { flow, pipe } from "@effect-app/prelude/Function"
 // Must export explicity for auto import to work
