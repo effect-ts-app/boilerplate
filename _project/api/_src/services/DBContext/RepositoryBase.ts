e5407787361fdff8f312c880b61fcf03e7fc8caf
Patrick Roza <contact@patrickroza.com>
2023-03-14 13:16:32 +0100

    improve: Repository handling

--- _project/api/_src/services/DBContext/RepositoryBase.ts
+++ _project/api/_src/services/DBContext/RepositoryBase.ts
@@ -1,23 +1,26 @@
 import type { RequestContext } from "@effect-app/infra/RequestContext"
 import type { Repository } from "@effect-app/infra/services/Repository"
-import type { ContextMap, Filter } from "@effect-app/infra/services/Store"
+import { ContextMap, StoreMaker } from "@effect-app/infra/services/Store"
+import type { Filter, Store, Where } from "@effect-app/infra/services/Store"
 import type { ParserEnv } from "@effect-app/schema/custom/Parser"
 import type { InvalidStateError, OptimisticConcurrencyException } from "api/errors.js"
-
 import type {} from "@effect/data/Equal"
 import type {} from "@effect/data/Hash"
-import type { Effect } from "@effect-app/core/Effect"
+import { Effect } from "@effect-app/core/Effect"
 import type { Opt } from "@effect-app/core/Option"
-import type { Chunk } from "@effect-app/prelude"
+import { makeCodec } from "@effect-app/infra/api/codec"
+import { makeFilters } from "@effect-app/infra/filter"
+import type { Chunk, Schema } from "@effect-app/prelude"
+import { EParserFor } from "@effect-app/prelude/schema"
 import { assignTag } from "@effect-app/prelude/service"
 
 export const RepositoryBase = <Service>() => {
-  return <T extends { id: Id }, PM extends { id: string }, Evt, Id extends string, ItemType extends string>(
+  return <T extends { id: string }, PM extends { id: string; _etag: string | undefined }, Evt, ItemType extends string>(
     itemType: ItemType
   ) => {
-    abstract class RepositoryBaseC implements Repository<T, PM, Evt, Id, ItemType> {
+    abstract class RepositoryBaseC implements Repository<T, PM, Evt, ItemType> {
       itemType: ItemType = itemType
-      abstract find: (id: Id) => Effect<ContextMap | RequestContext, never, Opt<T>>
+      abstract find: (id: T["id"]) => Effect<ContextMap | RequestContext, never, Opt<T>>
       abstract all: Effect<ContextMap, never, Chunk<T>>
       abstract saveAndPublish: (
         items: Iterable<T>,
@@ -32,7 +35,295 @@ export const RepositoryBase = <Service>() => {
         all: Effect<never, never, Chunk<PM>>
         filter: (filter: Filter<PM>, cursor?: { limit?: number; skip?: number }) => Effect<never, never, Chunk<PM>>
       }
+      static where = makeWhere<PM>()
     }
     return assignTag<Service>()(RepositoryBaseC)
+  }
+}
+
+export function makeRepo<
+  T extends { id: string },
+  PM extends { id: string; _etag: string | undefined },
+  Evt = never
+>() {
+  return <
+    ItemType extends string,
+    ConstructorInput,
+    Api,
+    E extends { id: string }
+  >(
+    name: ItemType,
+    schema: Schema.Schema<unknown, T, ConstructorInput, E, Api>,
+    mapFrom: (pm: Omit<PM, "_etag">) => E,
+    mapTo: (e: E, etag: string | undefined) => PM
+  ) => {
+    const where = makeWhere<PM>()
+
+    function mapToPersistenceModel(
+      e: E,
+      getEtag: (id: string) => string | undefined
+    ): PM {
+      return mapTo(e, getEtag(e.id))
+    }
+
+    function mapReverse(
+      { _etag, ...e }: PM,
+      setEtag: (id: string, eTag: string | undefined) => void
+    ): E {
+      setEtag(e.id, _etag)
+      return mapFrom(e)
+    }
+
+    const mkStore = makeStore<PM>()(name, schema, mapTo)
+
+    function make(
+      makeInitial: Effect<never, never, never[] | readonly [T, ...T[]]>,
+      publishEvents: (evt: Iterable<Evt>) => Effect<never, never, void>,
+      partitionValue: (a: PM) => string = _ => "primary" /*(isIntegrationEvent(r) ? r.companyId : r.id*/
+    ) {
+      return Do($ => {
+        const store = $(mkStore(makeInitial, partitionValue))
+
+        const allE = store.all.flatMap(items =>
+          Do($ => {
+            const { set } = $(ContextMap.access)
+            return items.map(_ => mapReverse(_, set))
+          })
+        )
+
+        const all = allE.flatMap(_ => _.forEachEffect(EParserFor(schema).condemnDie))
+
+        function findE(id: T["id"]) {
+          return store.find(id)
+            .flatMap(items =>
+              Do($ => {
+                const { set } = $(ContextMap.access)
+                return items.map(_ => mapReverse(_, set))
+              })
+            )
+        }
+
+        function find(id: T["id"]) {
+          return findE(id).flatMapOpt(EParserFor(schema).condemnDie)
+        }
+
+        const saveAllE = (a: Iterable<E>) =>
+          Effect(a.toNonEmptyArray)
+            .flatMapOpt(a =>
+              Do($ => {
+                const { get, set } = $(ContextMap.access)
+                const items = a.mapNonEmpty(_ => mapToPersistenceModel(_, get))
+                const ret = $(store.batchSet(items))
+                ret.forEach(_ => set(_.id, _._etag))
+              })
+            )
+
+        const saveAll = (a: Iterable<T>) => saveAllE(a.toChunk.map(Encoder.for(schema)))
+
+        const saveAndPublish = (items: Iterable<T>, _: Iterable<Evt> = []) =>
+          saveAll(items)
+            > publishEvents(_)
+
+        const r: Repository<T, PM, Evt, ItemType> = {
+          /**
+           * @internal
+           */
+          utils: { mapReverse, parse: Parser.for(schema).unsafe, filter: store.filter, all: store.all },
+          itemType: name,
+          find,
+          all,
+          saveAndPublish
+        }
+        return r
+      })
+    }
+
+    return {
+      make,
+      where
+    }
+  }
+}
+
+export function makeWhere<PM extends { id: string; _etag: string | undefined }>() {
+  const f_ = makeFilters<PM>()
+  type WhereFilter = typeof f_
+
+  function makeFilter_(filter: (f: WhereFilter) => Filter<PM>) {
+    return filter(f_)
+  }
+
+  function where(
+    makeWhere: (
+      f: WhereFilter
+    ) => Where | readonly [Where, ...Where[]],
+    mode?: "or" | "and"
+  ) {
+    return makeFilter_(f => {
+      const m = makeWhere ? makeWhere(f) : []
+      return ({
+        mode,
+        where: (Array.isArray(m) ? m as unknown as [Where, ...Where[]] : [m]) as readonly [Where, ...Where[]]
+      })
+    })
+  }
+  return where
+}
+
+const pluralize = (s: string) =>
+  s.endsWith("s")
+    ? s + "es"
+    : s.endsWith("y")
+    ? s.substring(0, s.length - 1) + "ies"
+    : s + "s"
+
+export function makeStore<
+  PM extends { id: string; _etag: string | undefined }
+>() {
+  return <
+    ItemType extends string,
+    T extends { id: string },
+    ConstructorInput,
+    Api,
+    E extends { id: string }
+  >(
+    name: ItemType,
+    schema: Schema.Schema<unknown, T, ConstructorInput, E, Api>,
+    mapTo: (e: E, etag: string | undefined) => PM
+  ) => {
+    const [_dec, _encode, encodeToMap] = makeCodec(schema)
+    const encodeToMapPM = flow(
+      encodeToMap,
+      _ =>
+        _.flatMap(map =>
+          Effect.gen(function*($) {
+            const { get } = yield* $(ContextMap.access)
+            return new Map(
+              [...map.entries()].map(([k, v]) => [k, mapToPersistenceModel(v, get)])
+            )
+          })
+        )
+    )
+
+    function mapToPersistenceModel(
+      e: E,
+      getEtag: (id: string) => string | undefined
+    ): PM {
+      return mapTo(e, getEtag(e.id))
+    }
+
+    function makeStore(
+      makeInitial: Effect<never, never, never[] | readonly [T, ...T[]]>,
+      partitionValue: (a: PM) => string = _ => "primary" /*(isIntegrationEvent(r) ? r.companyId : r.id*/
+    ) {
+      return Do($ => {
+        const { make } = $(StoreMaker.access)
+
+        const store = $(
+          make<PM, string, T["id"]>(
+            pluralize(name),
+            makeInitial.flatMap(encodeToMapPM).setupNamedRequest("initial"),
+            {
+              partitionValue
+            }
+          )
+        )
+        return store
+      })
+    }
+
+    return makeStore
+  }
+}
+
+export const RepositoryBaseImpl = <Service>() => {
+  return <
+    PM extends { id: string; _etag: string | undefined },
+    Evt = never
+  >() =>
+  <ItemType extends string, T extends { id: string }, ConstructorInput, Api, E extends { id: string }>(
+    itemType: ItemType,
+    schema: Schema.Schema<unknown, T, ConstructorInput, E, Api>,
+    mapFrom: (pm: Omit<PM, "_etag">) => E,
+    mapTo: (e: E, etag: string | undefined) => PM
+  ): Tag<Service> & {
+    new(
+      store: Store<PM, string>,
+      publishEvents: (evt: Iterable<Evt>) => Effect<RequestContext, never, void>
+    ): Repository<T, PM, Evt, ItemType>
+    makeStore: (
+      makeInitial: Effect<never, never, never[] | readonly [T, ...T[]]>,
+      partitionValue?: (a: PM) => string
+    ) => Effect<StoreMaker, never, Store<PM, string>>
+    where: ReturnType<typeof makeWhere<PM>>
+  } => {
+    return class extends RepositoryBase<Service>()<T, PM, Evt, ItemType>(itemType) {
+      constructor(
+        private readonly store: Store<PM, string>,
+        private readonly publishEvents: (evt: Iterable<Evt>) => Effect<RequestContext, never, void>
+      ) {
+        super()
+      }
+
+      static makeStore = makeStore<PM>()(itemType, schema, mapTo)
+
+      private mapToPersistenceModel = (
+        e: E,
+        getEtag: (id: string) => string | undefined
+      ): PM => {
+        return mapTo(e, getEtag(e.id))
+      }
+
+      private mapReverse = (
+        { _etag, ...e }: PM,
+        setEtag: (id: string, eTag: string | undefined) => void
+      ): E => {
+        setEtag(e.id, _etag)
+        return mapFrom(e)
+      }
+
+      private findE(id: T["id"]) {
+        return this.store.find(id)
+          .flatMap(items =>
+            Do($ => {
+              const { set } = $(ContextMap.access)
+              return items.map(_ => this.mapReverse(_, set))
+            })
+          )
+      }
+
+      override find = (id: T["id"]) => this.findE(id).flatMapOpt(EParserFor(schema).condemnDie)
+      private allE = this.store.all.flatMap(items =>
+        Do($ => {
+          const { set } = $(ContextMap.access)
+          return items.map(_ => this.mapReverse(_, set))
+        })
+      )
+
+      override all = this.allE.flatMap(_ => _.forEachEffect(EParserFor(schema).condemnDie))
+      private saveAllE = (a: Iterable<E>) =>
+        Effect(a.toNonEmptyArray)
+          .flatMapOpt(a =>
+            Do($ => {
+              const { get, set } = $(ContextMap.access)
+              const items = a.mapNonEmpty(_ => this.mapToPersistenceModel(_, get))
+              const ret = $(this.store.batchSet(items))
+              ret.forEach(_ => set(_.id, _._etag))
+            })
+          )
+
+      private saveAll = (a: Iterable<T>) => this.saveAllE(a.toChunk.map(Encoder.for(schema)))
+
+      override saveAndPublish = (items: Iterable<T>, _: Iterable<Evt> = []) =>
+        this.saveAll(items)
+          > this.publishEvents(_)
+
+      override utils = {
+        mapReverse: this.mapReverse,
+        parse: Parser.for(schema).unsafe,
+        filter: this.store.filter,
+        all: this.store.all
+      }
+    }
   }
 }
--- _project/api/_src/services/DBContext/UserRepo.ts
+++ _project/api/_src/services/DBContext/UserRepo.ts
@@ -1,68 +1,9 @@
-import type { UserId } from "@effect-app-boilerplate/models/User"
 import { User } from "@effect-app-boilerplate/models/User"
-import { makeCodec } from "@effect-app/infra/api/codec"
-import { makeFilters } from "@effect-app/infra/filter"
-import type { Filter, Where } from "@effect-app/infra/services/Store"
-import { ContextMap, StoreMaker } from "@effect-app/infra/services/Store"
 import { UserProfile } from "../UserProfile.js"
-import { RepositoryBase } from "./RepositoryBase.js"
+import { RepositoryBaseImpl } from "./RepositoryBase.js"
 
 export interface UserPersistenceModel extends User.Encoded {
   _etag: string | undefined
-}
-
-const f_ = makeFilters<UserPersistenceModel>()
-export type UserWhereFilter = typeof f_
-
-export function makeUserFilter_(filter: (f: UserWhereFilter) => Filter<UserPersistenceModel>) {
-  return filter(f_)
-}
-
-export function usersWhere(
-  makeWhere: (
-    f: UserWhereFilter
-  ) => Where | readonly [Where, ...Where[]],
-  mode?: "or" | "and"
-) {
-  return makeUserFilter_(f => {
-    const m = makeWhere ? makeWhere(f) : []
-    return ({
-      mode,
-      where: (Array.isArray(m) ? m as unknown as [Where, ...Where[]] : [m]) as readonly [Where, ...Where[]]
-    })
-  })
-}
-
-const [_dec, _encode, encodeToMap] = makeCodec(User)
-const encodeToMapPM = flow(
-  encodeToMap,
-  _ =>
-    _.flatMap(map =>
-      Effect.gen(function*($) {
-        const { get } = yield* $(ContextMap.access)
-        return new Map(
-          [...map.entries()].map(([k, v]) => [k, mapToPersistenceModel(v, get)])
-        )
-      })
-    )
-)
-
-function mapToPersistenceModel(
-  e: User.Encoded,
-  getEtag: (id: string) => string | undefined
-): UserPersistenceModel {
-  return {
-    ...e,
-    _etag: getEtag(e.id)
-  }
-}
-
-function mapReverse(
-  { _etag, ...e }: UserPersistenceModel,
-  setEtag: (id: string, eTag: string | undefined) => void
-): User.Encoded {
-  setEtag(e.id, _etag)
-  return e
 }
 
 const fakeUsers = ReadonlyArray.range(1, 8)
@@ -77,134 +18,31 @@ const fakeUsers = ReadonlyArray.range(1, 8)
 export type UserSeed = "sample" | ""
 
 /**
- * NOTE: Printer uniqueness handling is currently not optimal, and only uses a local lock,
- * so when running multiple instances, uniqueness is currently not guaranteed.
- */
-function makeUserRepo(seed: UserSeed) {
-  return Do($ => {
-    const { make } = $(StoreMaker.access)
-
-    const makeItems = Effect.sync(() => {
-      const items = seed === "sample" ? fakeUsers : []
-      return items
-    })
-
-    const store = $(
-      make<UserPersistenceModel, string, UserId>(
-        "Users",
-        makeItems.flatMap(encodeToMapPM).setupNamedRequest("initial"),
-        {
-          partitionValue: _ => "primary" /*(isIntegrationEvent(r) ? r.companyId : r.id*/
-        }
-      )
-    )
-
-    const allE = store.all.flatMap(items =>
-      Do($ => {
-        const { set } = $(ContextMap.access)
-        return items.map(_ => mapReverse(_, set))
-      })
-    )
-
-    const all = allE.flatMap(_ => _.forEachEffect(User.EParser.condemnDie))
-
-    function findE(id: UserId) {
-      return store.find(id)
-        .flatMap(items =>
-          Do($ => {
-            const { set } = $(ContextMap.access)
-            return items.map(_ => mapReverse(_, set))
-          })
-        )
-    }
-
-    function find(id: UserId) {
-      return findE(id).flatMapOpt(User.EParser.condemnDie)
-    }
-
-    // const saveE = (a: User.Encoded) =>
-    //   Do($ => {
-    //     const { get, set } = $(ContextMap.access)
-    //     const e = mapToPersistenceModel(a, get)
-
-    //     $(
-    //       permit.withPermit(
-    //         allE.flatMap(u =>
-    //           e.printer?.printerId &&
-    //             u.filter(_ => _.id !== e.id).some(v => v.printer?.printerId === e.printer!.printerId)
-    //             ? Effect.fail({
-    //               _tag: "IndexError" as const,
-    //               message: "Found duplicate printerId: " + e.printer.printerId
-    //             })
-    //             : store.set(e)
-    //         )
-    //           .flatMap(ret => Effect(set(ret.id, ret._etag)))
-    //       )
-    //     )
-    //   })
-
-    // const save = (a: User) => saveE(User.Encoder(a))
-
-    const saveAllE = (a: Iterable<User.Encoded>) =>
-      Effect(a.toNonEmptyArray)
-        .flatMapOpt(a =>
-          Do($ => {
-            const { get, set } = $(ContextMap.access)
-            const items = a.mapNonEmpty(_ => mapToPersistenceModel(_, get))
-            // TODO: Check duplicate printer
-            const ret = $(store.batchSet(items))
-            ret.forEach(_ => set(_.id, _._etag))
-          })
-        )
-
-    const saveAll = (a: Iterable<User>) => saveAllE(a.toChunk.map(User.Encoder))
-
-    const saveAndPublish = (items: Iterable<User>, _: Iterable<never> = []) => saveAll(items)
-    // .tap(() =>
-    //   Effect(items.toNonEmptyArray).tapOpt(items =>
-    //     // TODO: Poor Man's state change report; should perhaps auto track and filter for: ItemChanged, ItemStateChanged, or not changed.
-    //     publishClientEvent(ClientEvents.of.UserStatesUpdated({ items }))
-    //   )
-    // )
-    // .zipRight(
-    //   Effect(events.toNonEmptyArray)
-    //     // TODO: for full consistency the events should be stored within the same database transaction, and then picked up.
-    //     .flatMapOpt(publish)
-    // )
-
-    const r: UserRepo = {
-      /**
-       * @internal
-       */
-      utils: { mapReverse, parse: User.Parser.unsafe, filter: store.filter, all: store.all },
-      itemType: "User",
-      find,
-      all,
-      saveAndPublish
-      /*
-.catchTag(
-          "IndexError",
-          () => Effect.fail(new InvalidStateError(`Duplicate printer id ${u.printer?.printerId}`))
-        )
-      */
-    }
-    return r
-  })
-}
-
-/**
  * @tsplus type UserRepo
  * @tsplus companion UserRepo.Ops
  */
-export abstract class UserRepo
-  extends RepositoryBase<UserRepo>()<User, UserPersistenceModel, never, UserId, "User">("User")
-{}
+export class UserRepo extends RepositoryBaseImpl<UserRepo>()<UserPersistenceModel>()(
+  "User",
+  User,
+  pm => pm,
+  (e, _etag) => ({ ...e, _etag })
+) {}
+
+export const usersWhere = UserRepo.where
 
 /**
  * @tsplus static UserRepo.Ops Live
  */
 export function LiveUserRepo(seed: UserSeed) {
-  return makeUserRepo(seed).toLayer(UserRepo)
+  const makeInitial = Effect.sync(() => {
+    const items = seed === "sample" ? fakeUsers : []
+    return items
+  })
+  const publishEvents = (_: Iterable<never>) => Effect.unit
+  return UserRepo
+    .makeStore(makeInitial)
+    .map(_ => new UserRepo(_, publishEvents))
+    .toLayer(UserRepo)
 }
 
 /**
