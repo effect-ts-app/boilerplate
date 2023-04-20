22ea695948c0274cc4a58f5381903218a40741d3
Patrick Roza <contact@patrickroza.com>
2023-04-20 12:02:50 +0200

    housekeeping: update packages

--- _project/messages/_src/RequestLayers.ts
+++ _project/messages/_src/RequestLayers.ts
@@ -1,5 +1,3 @@
-import { RequestContext } from "@effect-app/infra/RequestContext"
-import { RequestContextContainer } from "@effect-app/infra/services/RequestContextContainer"
 import { ContextMap } from "@effect-app/infra/services/Store"
 
 export const BasicRequestEnv = Effect.gen(function*($) {
@@ -8,29 +6,11 @@ export const BasicRequestEnv = Effect.gen(function*($) {
   return rc
 })
 
-function makeInternalRequestContext(name: string) {
-  const id = StringId.make()
-  return new RequestContext({
-    id,
-    rootId: id,
-    locale: "en",
-    name: ReasonableString(name)
-  })
-}
-
 /**
  * @tsplus fluent effect/io/Effect setupNamedRequest
  */
 export function setupReq2<R, E, A>(self: Effect<R, E, A>, name: string) {
-  return self.setupNamedRequest3(makeInternalRequestContext(name))
-}
-
-/**
- * @tsplus fluent effect/io/Effect setupNamedRequest3
- */
-export function setupReq3<R, E, A>(self: Effect<R, E, A>, requestContext: RequestContext) {
   return self
-    .setupRequestFrom
     .provideSomeContextEffect(BasicRequestEnv)
-    .provideService(RequestContextContainer, RequestContextContainer.live(requestContext))
+    .setupRequestFromWith(name)
 }
