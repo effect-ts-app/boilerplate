2bc75b9b78f8c7784c76c16d9ed9e8340ef87511
Patrick Roza <contact@patrickroza.com>
2024-01-31 10:09:57 +0100

    update

--- _project/api/_src/services/Events.ts
+++ _project/api/_src/services/Events.ts
@@ -1,10 +1,12 @@
 import type { ClientEvents } from "@effect-app-boilerplate/resources"
 import { storeId } from "@effect-app/infra/services/Store/Memory"
+import { PubSub } from "effect"
 
 const makeEvents = Effect.gen(function*($) {
   const q = yield* $(PubSub.unbounded<{ evt: ClientEvents; namespace: string }>())
-  const svc: Events = {
-    publish: (...evts) => storeId.get.flatMap((namespace) => q.offerAll(evts.map((evt) => ({ evt, namespace })))),
+  const svc = {
+    publish: (...evts: NonEmptyReadonlyArray<ClientEvents>) =>
+      storeId.get.flatMap((namespace) => q.offerAll(evts.map((evt) => ({ evt, namespace })))),
     subscribe: q.subscribe,
     stream: Stream.fromPubSub(q)
   }
@@ -15,14 +17,6 @@ const makeEvents = Effect.gen(function*($) {
  * @tsplus type Events
  * @tsplus companion Events.Ops
  */
-export class Events extends TagClass<Events, {
-  publish: (...events: NonEmptyReadonlyArray<ClientEvents>) => Effect<never, never, void>
-  subscribe: Effect<Scope, never, Dequeue<{ evt: ClientEvents; namespace: string }>>
-  stream: Stream<never, never, { evt: ClientEvents; namespace: string }>
-}>() {
+export abstract class Events extends TagClassMake<Events>()(makeEvents) {
+  static readonly Live = this.toLayer()
 }
-
-/**
- * @tsplus static Events.Ops Live
- */
-export const LiveEvents = makeEvents.toLayer(Events)
