5c7fb417bb5583d49b19b0ef1576e8b8a4441173
Patrick Roza <contact@patrickroza.com>
2024-02-03 13:30:21 +0100

    Switch to BlogPostView and UserView, resolving the users automatically on the client.

--- _project/resources/_src/Views.ts
+++ _project/resources/_src/Views.ts
@@ -1,5 +1,6 @@
 // codegen:start {preset: barrel, include: ./Views/*.ts}
-
+export * from "./Views/PostView.js"
+export * from "./Views/UserView.js"
 // codegen:end
 
 export * from "@effect-app/prelude/Operations"
---
+++ _project/resources/_src/Views/PostView.ts
@@ -0,0 +1,22 @@
+import { BlogPost } from "@effect-app-boilerplate/models/Blog"
+import { UserViewFromId } from "../resolvers/UserResolver.js"
+
+@useClassFeaturesForSchema
+export class BlogPostView extends ExtendedClass<BlogPostView.From, BlogPostView>()({
+  ...BlogPost.fields.$$.omit("user"),
+  user: UserViewFromId.mapFrom("userId")
+}) {}
+
+// codegen:start {preset: model}
+//
+/* eslint-disable */
+export namespace BlogPostView {
+  /**
+   * @tsplus type BlogPostView.From
+   * @tsplus companion BlogPostView.From/Ops
+   */
+  export class From extends FromClass<typeof BlogPostView>() {}
+}
+/* eslint-enable */
+//
+// codegen:end
