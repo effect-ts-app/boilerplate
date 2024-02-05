556b81744af5b5248ccba44ed77caad70c0be2d0
Patrick Roza <contact@patrickroza.com>
2024-02-05 09:58:53 +0100

    cleanup

--- _project/resources/_src/Blog/PublishPost.ts
+++ _project/resources/_src/Blog/PublishPost.ts
@@ -1,5 +1,5 @@
 import { BlogPostId } from "@effect-app-boilerplate/models/Blog"
-import { S } from "@effect-app/prelude"
+import { S } from "@effect-app-boilerplate/resources/lib"
 import { OperationId } from "../Views.js"
 
 export class PublishPostRequest extends S.Req({ allowAnonymous: true, allowRoles: ["user"] })<PublishPostRequest>()({
