9144f3f419a574c96fce05753c9ac922850ff076
Patrick Roza <contact@patrickroza.com>
2024-02-05 10:39:55 +0100

    fix l

--- scripts/link.js
+++ scripts/link.js
@@ -2,14 +2,14 @@ import pj from '../package.json'  assert { type: 'json' };
 
 pj.resolutions = {
   ...pj.resolutions,
-  "@effect-app/core": "file:../effect-app/libs/packages/core",
-  "@effect-app/eslint-codegen-model": "file:../effect-app/libs/packages/eslint-codegen-model",
-  "@effect-app/prelude": "file:../effect-app/libs/packages/prelude",
-  "@effect-app/fluent-extensions": "file:../effect-app/libs/packages/fluent-extensions",
-  "@effect-app/infra": "file:../effect-app/libs/packages/infra",
-  "@effect-app/infra-adapters": "file:../effect-app/libs/packages/infra-adapters",
-  "@effect-app/schema": "file:../effect-app/libs/packages/schema",
-  "@effect-app/vue": "file:../effect-app/libs/packages/vue"
+  "@effect-app/core": "file:../libs/packages/core",
+  "@effect-app/eslint-codegen-model": "file:../libs/packages/eslint-codegen-model",
+  "@effect-app/prelude": "file:../libs/packages/prelude",
+  "@effect-app/fluent-extensions": "file:../libs/packages/fluent-extensions",
+  "@effect-app/infra": "file:../libs/packages/infra",
+  "@effect-app/infra-adapters": "file:../libs/packages/infra-adapters",
+  "@effect-app/schema": "file:../libs/packages/schema",
+  "@effect-app/vue": "file:../libs/packages/vue"
 }
 
 import fs from "fs"
