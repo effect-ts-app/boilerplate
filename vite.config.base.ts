6100926fa6ce1890808435fc50dabc5cd5a74e61
Patrick Roza <contact@patrickroza.com>
2024-02-07 18:47:01 +0100

    dx: cleanup configs

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -1,25 +1,37 @@
 /// <reference types="vitest" />
-import path from "path"
-import fs from "fs"
 import { effectPlugin } from "@effect-app/compiler/vitePlugin2"
-import { UserConfig } from "vite"
+import fs from "fs"
+import path from "path"
+import type { UserConfig } from "vite"
 
-export default function makeConfig(dirName?: string, useDist = process.env.TEST_USE_DIST === "true"): UserConfig {
-
+export default function makeConfig(
+  dirName?: string,
+  useDist = process.env.TEST_USE_DIST === "true",
+  useTransform = true
+): UserConfig {
+  const d = dirName ? dirName + "/" : ""
   return {
-    plugins: useDist ? [] : [effectPlugin({})],
+    plugins: useDist
+      ? []
+      : useTransform
+      ? [effectPlugin({ tsconfig: dirName ? d + "tsconfig.json" : undefined })]
+      : [],
     test: {
       include: useDist ? ["./dist/**/*.test.js"] : ["./_src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
       exclude: ["./_test/**/*"],
       reporters: "verbose",
       globals: true,
-      deps: useDist ? { inline: [new RegExp(dirName + "/dist")], } : undefined,
+      deps: useDist ? { inline: [new RegExp(dirName + "/dist")] } : undefined
     },
     resolve: dirName
       ? {
         alias: {
-          [JSON.parse(fs.readFileSync(dirName + "/package.json", "utf-8")).name]: path.resolve(dirName, useDist ? "/dist" : "/_src")
-        } }
-      : undefined,
+          [JSON.parse(fs.readFileSync(dirName + "/package.json", "utf-8")).name]: path.join(
+            dirName,
+            useDist ? "/dist" : "/_src"
+          )
+        }
+      }
+      : undefined
   }
 }
--- vite.config.test.ts
+++ vite.config.test.ts
@@ -1,24 +1,34 @@
 /// <reference types="vitest" />
 import { effectPlugin } from "@effect-app/compiler/vitePlugin2"
 import fs from "fs"
-import makeConfig from "./vite.config.base"
 import AutoImport from "unplugin-auto-import/vite"
-import { UserConfig } from "vite"
 import { defineConfig } from "vite"
+import type { UserConfig } from "vite"
+import makeConfig from "./vite.config.base"
 
 export default function defineTestConfig(
   dirName?: string,
-  transform?: (cfg: UserConfig, useDist: boolean, useFullDist: boolean)  => UserConfig,
-  useDist = process.env.TEST_USE_DIST === "true",
-  useFullDist = process.env.TEST_USE_FULL_DIST === "true",
+  transform?: (cfg: UserConfig, useDist: boolean, useFullDist: boolean) => UserConfig,
+  options: { useTransform?: boolean; useFullDist?: boolean; useDist?: boolean } = {
+    useTransform: true,
+    useDist: process.env.TEST_USE_DIST === "true",
+    useFullDist: process.env.TEST_USE_FULL_DIST === "true"
+  }
 ) {
+  let {
+    useDist = process.env.TEST_USE_DIST === "true",
+    // eslint-disable-next-line prefer-const
+    useFullDist = process.env.TEST_USE_FULL_DIST === "true",
+    // eslint-disable-next-line prefer-const
+    useTransform = true
+  } = options
   if (useFullDist) {
     useDist = true
   }
-  const b = makeConfig(dirName, useDist)
+  const b = makeConfig(dirName, useDist, useTransform)
   // autoimport seems to work best, even if in some cases setting vitest/globals as types works.
   const autoImport = AutoImport({
-    dts: './_test/auto-imports.d.ts',
+    dts: "./_test/auto-imports.d.ts",
     // include: [
     //   /\.test\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
     // ],
@@ -26,18 +36,33 @@ export default function defineTestConfig(
       "vitest"
     ]
   })
+
+  const d = dirName ? dirName + "/" : ""
   const cfg = {
     ...b,
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    plugins: useFullDist ? [autoImport] : [effectPlugin({ tsconfig: fs.existsSync("tsconfig.test.local.json") ? "tsconfig.test.local.json" : "tsconfig.test.json" }), autoImport],
+    plugins: useFullDist
+      ? [autoImport]
+      : [
+        ...useTransform
+          ? [effectPlugin({
+            tsconfig: fs.existsSync(d + "tsconfig.test.local.json")
+              ? d + "tsconfig.test.local.json"
+              : d + "tsconfig.test.json"
+          })]
+          : [],
+        autoImport
+      ],
     test: {
-      ...b.test, 
-      include: useFullDist ? ["./_test/dist/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] : ["./_test/**/*.test.{ts,mts,cts,jsx,tsx}"],
-      exclude: []
+      ...b.test,
+      include: useFullDist
+        ? ["./dist/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"]
+        : ["./**/*.test.{ts,mts,cts,jsx,tsx}"],
+      exclude: ["**/node_modules/**"]
     },
-    //watchExclude: ["**/node_modules/**"],
-    //forceRerunTriggers: ['**/package.json/**', '**/vitest.config.*/**', '**/vite.config.*/**', '**/dist/**']
+    watchExclude: ["**/node_modules/**"]
+    // forceRerunTriggers: ['**/package.json/**', '**/vitest.config.*/**', '**/vite.config.*/**', '**/dist/**']
   }
-  //console.log("cfg", cfg)
+  // console.log("cfg", cfg)
   return defineConfig(transform ? transform(cfg, useDist, useFullDist) : cfg)
 }
