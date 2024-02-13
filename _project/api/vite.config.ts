b30a2c86d303a64f6fb92cdbb7badd5db95dbc01
Patrick Roza <contact@patrickroza.com>
2024-02-13 09:24:14 +0100

    vite

--- _project/api/vite.config.ts
+++ _project/api/vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "vite"
 import makeConfig from "../../vite.config.base"
 
 const cfg = makeConfig(__dirname)
-console.log("cfg", cfg)
+// console.log("cfg", cfg)
 export default defineConfig(cfg)
--- vite.config.base.ts
+++ vite.config.base.ts
@@ -1,5 +1,4 @@
 /// <reference types="vitest" />
-import { effectPlugin } from "@effect-app/compiler/vitePlugin2"
 import fs from "fs"
 import path from "path"
 import type { UserConfig } from "vite"
@@ -17,13 +16,18 @@ export default function makeConfig(
   const alias = (name: string) => ({
     [basePj + "/" + name]: path.join(__dirname, `/_project/${name}/` + (useDist || useTransform ? "dist" : "_src"))
   })
+  const projects = ["api", "core", "messages", "resources", "models"]
   const d = dirName ? dirName + "/" : ""
   return {
     plugins: useDist
-      ? [tsconfigPaths()]
+      ? []
       : useTransform
-      ? [effectPlugin({ tsconfig: dirName ? d + "tsconfig.json" : undefined })]
-      : [tsconfigPaths()],
+      ? [
+        require("@effect-app/compiler/vitePlugin2").effectPlugin({
+          tsconfig: dirName ? d + "tsconfig.json" : undefined
+        })
+      ]
+      : [tsconfigPaths({ projects: projects.map((_) => path.join(__dirname, `/_project/${_}`)) })],
     test: {
       include: useDist ? ["./dist/**/*.test.js"] : ["./_src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
       exclude: ["./_test/**/*"],
@@ -33,7 +37,10 @@ export default function makeConfig(
     resolve: dirName
       ? {
         alias: {
-          ...["api", "core", "messages", "resources", "models"].reduce((acc, cur) => ({ ...acc, ...alias(cur) }), {}),
+          ...projects.reduce(
+            (acc, cur) => ({ ...acc, ...alias(cur) }),
+            {}
+          ),
           [JSON.parse(fs.readFileSync(dirName + "/package.json", "utf-8")).name]: path.join(
             dirName,
             useDist ? "/dist" : "/_src"
