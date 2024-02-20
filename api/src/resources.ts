2359b65f6ffd7b915abcc1baf40fcd286d46be50
Patrick Roza <contact@patrickroza.com>
2024-02-20 14:20:19 +0100

    fun

--- api/src/resources.ts
+++ api/src/resources.ts
@@ -3,10 +3,11 @@ import type {} from "@effect/platform/Http/Client"
 
 export { ClientEvents } from "./resources/Events.js"
 
-// codegen:start {preset: barrel, include: ./resources/*.ts, exclude: [./resources/_global*.ts, ./resources/index.ts, ./resources/lib.ts, ./resources/integrationEvents.ts, ./resources/Messages.ts, ./resources/Views.ts, ./resources/errors.ts, ./resources/Events.ts], export: { as: 'PascalCase', postfix: 'Rsc' }}
-export * as BlogRsc from "./resources/Blog.js"
-export * as HelloWorldRsc from "./resources/HelloWorld.js"
-export * as MeRsc from "./resources/Me.js"
-export * as OperationsRsc from "./resources/Operations.js"
-export * as UsersRsc from "./resources/Users.js"
+// codegen:start {preset: barrel, include: ./Usecases/*.ts, exclude: [./Usecases/*Controllers.ts, ./resources/_global*.ts, ./resources/index.ts, ./resources/lib.ts, ./resources/integrationEvents.ts, ./resources/Messages.ts, ./resources/Views.ts, ./resources/errors.ts, ./resources/Events.ts], export: { as: 'PascalCase', postfix: 'Rsc' }}
+export * as BlogRsc from "./Usecases/Blog.js"
+export * as HelloWorldRsc from "./Usecases/HelloWorld.js"
+export * as MeRsc from "./Usecases/Me.js"
+export * as MessagesRsc from "./Usecases/Messages.js"
+export * as OperationsRsc from "./Usecases/Operations.js"
+export * as UsersRsc from "./Usecases/Users.js"
 // codegen:end
