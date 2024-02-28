78b4d3e6c8ba70865484553ef2f87cbdbb7e3398
Patrick Roza <contact@patrickroza.com>
2024-02-28 21:42:10 +0100

    cleanup

--- api/src/services.ts
+++ api/src/services.ts
@@ -1,11 +1,6 @@
 // codegen:start {preset: barrel, include: services/*.ts }
 export * from "./services/DBContext.js"
 export * from "./services/Events.js"
+export * from "./services/lib.js"
 export * from "./services/UserProfile.js"
 // codegen:end
-
-export * from "@effect-app/infra-adapters/memQueue"
-export * from "@effect-app/infra-adapters/ServiceBus"
-export * from "@effect-app/infra/services/Emailer"
-export * from "@effect-app/infra/services/Operations"
-export * from "@effect-app/infra/services/Store/index"
---
+++ api/src/services/lib.ts
@@ -0,0 +1,7 @@
+export * from "@effect-app/infra-adapters/memQueue"
+export * from "@effect-app/infra-adapters/ServiceBus"
+export * from "@effect-app/infra/services/Emailer"
+export * from "@effect-app/infra/services/Operations"
+export * from "@effect-app/infra/services/Store/index"
+
+export * as Q from "@effect-app/infra/services/query"
