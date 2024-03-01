fe231f3f8990c4544a153c6760aea61e85da6e80
Patrick Roza <contact@patrickroza.com>
2024-03-01 10:32:25 +0100

    housekeeping: update packages

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,4 @@
-import { Effect } from "effect-app"
 import { matchAll } from "./lib/matchFor.js"
 import * as Controllers from "./Usecases.js"
 
-export const all = Effect.all(matchAll(Controllers))
+export const router = matchAll(Controllers)
