2205a084235ec3d92a12e5d3eb822a851ff483b2
Patrick Roza <contact@patrickroza.com>
2024-03-03 10:23:36 +0100

    housekeeping: update packages

--- api/src/middleware/events.ts
+++ api/src/middleware/events.ts
@@ -14,7 +14,7 @@ export const events = Effect
     // Tell the client to retry every 10 seconds if connectivity is lost
     const setRetry = Stream.succeed("retry: 10000")
     const keepAlive = Stream.schedule(Effect.succeed(":keep-alive"), Schedule.fixed(Duration.seconds(15)))
-    const events = yield* $(Events.$.stream)
+    const events = yield* $(Events.stream)
 
     const stream = setRetry
       .pipe(
