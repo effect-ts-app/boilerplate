6330f82bb8e978719a2d7d7913c17e170fa44ba9
Patrick Roza <contact@patrickroza.com>
2024-04-03 17:55:20 +0200

    cleanup event stream

--- api/src/middleware/events.ts
+++ api/src/middleware/events.ts
@@ -1,9 +1,13 @@
 import { setupRequestContext } from "@effect-app/infra/api/setupRequest"
 import { reportError } from "@effect-app/infra/errorReporter"
-import { Duration, Effect, S, Schedule, Stream } from "effect-app"
+import { Duration, Effect, pipe, S, Schedule, Stream } from "effect-app"
 import { HttpHeaders, HttpServerResponse } from "effect-app/http"
 import { ClientEvents } from "resources.js"
 import { Events } from "../services/Events.js"
+
+// Tell the client to retry every 10 seconds if connectivity is lost
+const setRetry = Stream.succeed("retry: 10000")
+const keepAlive = Stream.schedule(Effect.succeed(":keep-alive"), Schedule.fixed(Duration.seconds(15)))
 
 export const events = Effect
   .gen(function*($) {
@@ -11,24 +15,19 @@ export const events = Effect
 
     const enc = new TextEncoder()
 
-    // Tell the client to retry every 10 seconds if connectivity is lost
-    const setRetry = Stream.succeed("retry: 10000")
-    const keepAlive = Stream.schedule(Effect.succeed(":keep-alive"), Schedule.fixed(Duration.seconds(15)))
     const events = yield* $(Events.stream)
+    const eventStream = Stream.map(
+      events,
+      (_) => `id: ${_.evt.id}\ndata: ${JSON.stringify(S.encodeSync(ClientEvents)(_.evt))}`
+    )
 
-    const stream = setRetry
-      .pipe(
-        Stream.merge(keepAlive),
-        Stream.merge(
-          Stream.map(events, (_) => `id: ${_.evt.id}\ndata: ${JSON.stringify(S.encodeSync(ClientEvents)(_.evt))}`)
-        ),
-        Stream.map((_) => enc.encode(_ + "\n\n"))
-      )
+    const stream = pipe(setRetry, Stream.merge(keepAlive), Stream.merge(eventStream))
 
     const ctx = yield* $(Effect.context<never>())
     const res = HttpServerResponse.stream(
       stream
         .pipe(
+          Stream.map((_) => enc.encode(_ + "\n\n")),
           Stream.tapErrorCause(reportError("Request")),
           Stream.provideContext(ctx)
         ),
