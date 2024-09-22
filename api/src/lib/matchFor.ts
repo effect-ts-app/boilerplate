6698c484fc14b8f200c292938626ca8bd8f45bdf
Patrick Roza <contact@patrickroza.com>
2024-09-22 02:12:48 +0200

    refactor: improve doc

--- api/src/lib/matchFor.ts
+++ api/src/lib/matchFor.ts
@@ -1,7 +1,8 @@
+import type { ContextMapInverted } from "@effect-app/infra/router"
 import { makeRouter } from "@effect-app/infra/router"
 import type { UserProfile } from "api/services.js"
 import { type CTX, handleRequestEnv } from "./RequestEnv.js"
 
-type CTXMap = { allowAnonymous: ["userProfile", UserProfile, false] }
+type CTXMap = { allowAnonymous: ContextMapInverted<"userProfile", UserProfile> }
 
 export const { matchAll, matchFor } = makeRouter<CTX, CTXMap>(handleRequestEnv)
