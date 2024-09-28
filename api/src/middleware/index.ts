97140496c8f989c1a69cbba93ef537806d2d3dbd
Patrick Roza <contact@patrickroza.com>
2024-09-28 18:45:41 +0200

    refactor: extract low level middleware

--- api/src/middleware/index.ts
+++ api/src/middleware/index.ts
@@ -2,6 +2,7 @@
 export * from "./auth.js"
 export * from "./events.js"
 export * from "./health.js"
-export * from "./Middlewares.js"
 export * from "./RequestContextMiddleware.js"
 // codegen:end
+
+export * from "@effect-app/infra/api/middlewares"
