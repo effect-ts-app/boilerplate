8d7c6fbb110391d05894b74c3532bd12eb799ae5
Patrick Roza <contact@patrickroza.com>
2024-10-06 21:30:32 +0200

    doc

--- api/src/resources/resolvers/UserResolver.ts
+++ api/src/resources/resolvers/UserResolver.ts
@@ -36,6 +36,7 @@ const getUserViewByIdResolver = RequestResolver
   )
   .pipe(RequestResolver.batchN(25), RequestResolver.contextFromServices(HttpClient.HttpClient, ApiConfig.Tag))
 
+// TODO: How to globally cache - right now we had to move the RequestCache from the runtime to clientFor
 export const UserViewFromId: Schema<UserView, string, ApiConfig | HttpClient.HttpClient.Service> = S.transformOrFail(
   UserId,
   S.typeSchema(UserView),
