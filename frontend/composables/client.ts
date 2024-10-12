22340ee28b5339a4669a4c937a7f4f16ac74845a
Patrick Roza <contact@patrickroza.com>
2024-10-12 16:49:48 +0200

    refactor: provide Runtime from outside

--- frontend/composables/client.ts
+++ frontend/composables/client.ts
@@ -1,18 +1,14 @@
-import { makeClient } from "@effect-app/vue"
+import { makeClient, makeMutation, makeQuery } from "@effect-app/vue"
 
 import { useToast } from "vue-toastification"
 import { useIntl } from "./intl"
+import { runtime, type RT } from "~/plugins/runtime"
+import type { Runtime } from "effect-app"
 
 export { useToast } from "vue-toastification"
 
 export { clientFor } from "resources/lib"
-export {
-  useSafeMutation,
-  useSafeQuery,
-  Result,
-  type MutationResult,
-  makeContext,
-} from "@effect-app/vue"
+export { Result, type MutationResult, makeContext } from "@effect-app/vue"
 export {
   pauseWhileProcessing,
   useIntervalPauseWhileProcessing,
@@ -20,11 +16,16 @@ export {
   withSuccessE,
   composeQueries,
   SuppressErrors,
-  useSafeMutationWithState,
   mapHandler,
 } from "@effect-app/vue/makeClient"
 
-export const { makeUseAndHandleMutation, useAndHandleMutation } = makeClient(
-  useIntl,
-  useToast,
-)
+// sue me
+const rt = computed(() => runtime.value?.runtime as Runtime.Runtime<RT>)
+export const useSafeQuery = makeQuery(rt)
+export const useSafeMutation = makeMutation(rt)
+
+export const {
+  makeUseAndHandleMutation,
+  useAndHandleMutation,
+  useSafeMutationWithState,
+} = makeClient(useIntl, useToast, useSafeMutation)
