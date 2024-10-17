86498d9d5f90966efb72f571d75ed8a26ce99100
Patrick Roza <contact@patrickroza.com>
2024-10-17 22:05:19 +0200

    restore

--- api/src/Users.controllers.ts
+++ api/src/Users.controllers.ts
@@ -5,19 +5,20 @@ import { Effect, Order } from "effect-app"
 import { UsersRsc } from "resources.js"
 import type { UserView } from "resources/views.js"
 
-export default matchFor(UsersRsc)(
+const router = matchFor(UsersRsc)
+
+export default router.effect(
   [UserRepo.Default],
-  ({ IndexUsers }) =>
-    Effect.gen(function*() {
-      const userRepo = yield* UserRepo
-      return {
-        IndexUsers: IndexUsers((req) =>
-          userRepo
-            .query(Q.where("id", "in", req.filterByIds))
-            .pipe(Effect.andThen((users) => ({
-              users: Array.sort(users, Order.mapInput(Order.string, (_: UserView) => _.displayName))
-            })))
-        )
-      }
-    })
+  Effect.gen(function*() {
+    const userRepo = yield* UserRepo
+    return {
+      IndexUsers: class extends router.IndexUsers((req) =>
+        userRepo
+          .query(Q.where("id", "in", req.filterByIds))
+          .pipe(Effect.andThen((users) => ({
+            users: Array.sort(users, Order.mapInput(Order.string, (_: UserView) => _.displayName))
+          })))
+      ) {}
+    }
+  })
 )
