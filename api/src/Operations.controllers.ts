f28b5604780a0822de0ab846ebc788485100bac0
Patrick Roza <contact@patrickroza.com>
2024-10-20 13:01:31 +0200

    refactor: save the indents!

--- api/src/Operations.controllers.ts
+++ api/src/Operations.controllers.ts
@@ -4,18 +4,17 @@ import { Effect } from "effect-app"
 import { OperationsRsc } from "resources.js"
 import { OperationsDefault } from "./lib/layers.js"
 
-export default matchFor(OperationsRsc)(
-  [OperationsDefault],
-  ({ FindOperation }) =>
-    Effect.gen(function*() {
-      const operations = yield* Operations
-      return {
-        FindOperation: FindOperation(
-          ({ id }) =>
-            operations
-              .find(id)
-              .pipe(Effect.andThen((_) => _.value ?? null))
-        )
-      }
-    })
-)
+export default matchFor(OperationsRsc)([
+  OperationsDefault
+], ({ FindOperation }) =>
+  Effect.gen(function*() {
+    const operations = yield* Operations
+    return {
+      FindOperation: FindOperation(
+        ({ id }) =>
+          operations
+            .find(id)
+            .pipe(Effect.andThen((_) => _.value ?? null))
+      )
+    }
+  }))
