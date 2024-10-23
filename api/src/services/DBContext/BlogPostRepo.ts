b94a8c1788cb504296973629fcefb5d9cb2159ae
Patrick Roza <contact@patrickroza.com>
2024-10-23 15:31:18 +0200

    refactor: define repo as a standard service.

--- api/src/services/DBContext/BlogPostRepo.ts
+++ api/src/services/DBContext/BlogPostRepo.ts
@@ -1,4 +1,4 @@
-import { RepositoryDefaultImpl2 } from "@effect-app/infra/services/RepositoryBase"
+import { Model } from "@effect-app/infra"
 import { RepoDefault } from "api/lib/layers.js"
 import { Effect } from "effect"
 import { NonEmptyString255, NonEmptyString2k } from "effect-app/Schema"
@@ -7,36 +7,33 @@ import { UserRepo } from "./UserRepo.js"
 
 export type BlogPostSeed = "sample" | ""
 
-export class BlogPostRepo extends RepositoryDefaultImpl2<BlogPostRepo>()(
-  "BlogPost",
-  BlogPost,
-  {
-    dependencies: [RepoDefault, UserRepo.Default, UserRepo.UserFromIdLayer],
-    options: Effect.gen(function*() {
-      const seed = "sample"
-      const userRepo = yield* UserRepo
+export class BlogPostRepo extends Effect.Service<BlogPostRepo>()("BlogPostRepo", {
+  dependencies: [RepoDefault, UserRepo.Default, UserRepo.UserFromIdLayer],
+  effect: Effect.gen(function*() {
+    const seed = "sample"
+    const userRepo = yield* UserRepo
 
-      const makeInitial = yield* Effect.cached(
-        seed === "sample"
-          ? userRepo
-            .all
-            .pipe(
-              Effect.andThen((users) =>
-                users
-                  .flatMap((_) => [_, _])
-                  .map((user, i) =>
-                    new BlogPost({
-                      title: NonEmptyString255("Test post " + i),
-                      body: NonEmptyString2k("imma test body"),
-                      author: user
-                    }, true)
-                  )
-              )
+    const makeInitial = yield* Effect.cached(
+      seed === "sample"
+        ? userRepo
+          .all
+          .pipe(
+            Effect.andThen((users) =>
+              users
+                .flatMap((_) => [_, _])
+                .map((user, i) =>
+                  new BlogPost({
+                    title: NonEmptyString255("Test post " + i),
+                    body: NonEmptyString2k("imma test body"),
+                    author: user
+                  }, true)
+                )
             )
-          : Effect.succeed([])
-      )
-      return { makeInitial }
-    })
-  }
-) {
+          )
+        : Effect.succeed([])
+    )
+
+    return yield* Model.makeRepo("BlogPost", BlogPost, { makeInitial })
+  })
+}) {
 }
