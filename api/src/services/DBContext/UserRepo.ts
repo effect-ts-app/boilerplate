d1e8c2bc260b0c9fe880dd377c3c23f11e652abe
Patrick Roza <contact@patrickroza.com>
2024-10-23 15:28:59 +0200

    refactor: define repo as a standard service.

--- api/src/services/DBContext/UserRepo.ts
+++ api/src/services/DBContext/UserRepo.ts
@@ -1,5 +1,5 @@
+import { Model } from "@effect-app/infra"
 import { NotFoundError, NotLoggedInError } from "@effect-app/infra/errors"
-import { RepositoryDefaultImpl2 } from "@effect-app/infra/services/RepositoryBase"
 import { generate } from "@effect-app/infra/test"
 import { RepoConfig } from "api/config.js"
 import { RepoDefault } from "api/lib/layers.js"
@@ -18,67 +18,45 @@ export interface UserPersistenceModel extends User.From {
 
 export type UserSeed = "sample" | ""
 
-export class UserRepo extends RepositoryDefaultImpl2<UserRepo>()(
-  "User",
-  User,
-  {
-    dependencies: [RepoDefault],
-    options: Effect.gen(function*() {
-      const cfg = yield* RepoConfig
+export class UserRepo extends Effect.Service<UserRepo>()("UserRepo", {
+  dependencies: [RepoDefault],
+  effect: Effect.gen(function*() {
+    const cfg = yield* RepoConfig
 
-      const makeInitial = yield* Effect.cached(Effect.sync(() => {
-        const seed = cfg.fakeUsers === "seed" ? "seed" : cfg.fakeUsers === "sample" ? "sample" : ""
-        const fakeUsers = pipe(
-          Array
-            .range(1, 8)
-            .map((_, i): User => {
-              const g = generate(S.A.make(User)).value
-              const emailArb = fakerArb((_) => () =>
-                _
-                  .internet
-                  .exampleEmail({ firstName: g.name.firstName, lastName: g.name.lastName })
-              )
-              return new User({
-                ...g,
-                email: Email(generate(emailArb(fc)).value),
-                role: i === 0 || i === 1 ? "manager" : "user"
-              })
-            }),
-          Array.toNonEmptyArray,
-          Option
-            .match({
-              onNone: () => {
-                throw new Error("must have fake users")
-              },
-              onSome: (_) => _
+    const makeInitial = yield* Effect.cached(Effect.sync(() => {
+      const seed = cfg.fakeUsers === "seed" ? "seed" : cfg.fakeUsers === "sample" ? "sample" : ""
+      const fakeUsers = pipe(
+        Array
+          .range(1, 8)
+          .map((_, i): User => {
+            const g = generate(S.A.make(User)).value
+            const emailArb = fakerArb((_) => () =>
+              _
+                .internet
+                .exampleEmail({ firstName: g.name.firstName, lastName: g.name.lastName })
+            )
+            return new User({
+              ...g,
+              email: Email(generate(emailArb(fc)).value),
+              role: i === 0 || i === 1 ? "manager" : "user"
             })
-        )
-        const items = seed === "sample" ? fakeUsers : []
-        return items
-      }))
+          }),
+        Array.toNonEmptyArray,
+        Option
+          .match({
+            onNone: () => {
+              throw new Error("must have fake users")
+            },
+            onSome: (_) => _
+          })
+      )
+      const items = seed === "sample" ? fakeUsers : []
+      return items
+    }))
 
-      return { makeInitial }
-    })
-  }
-) {
-  static readonly UserFromIdLayer = User
-    .resolver
-    .toLayer(
-      Effect
-        .andThen(this, (userRepo) =>
-          getUserByIdResolver
-            .pipe(
-              Effect.provideService(this, userRepo),
-              Effect.map((resolver) => ({
-                get: (id: UserId) =>
-                  Effect
-                    .request(GetUserById({ id }), resolver)
-                    .pipe(Effect.orDie)
-              }))
-            ))
-    )
-    .pipe(Layer.provide(this.Default))
-
+    return yield* Model.makeRepo("User", User, { makeInitial })
+  })
+}) {
   get tryGetCurrentUser() {
     return Effect.serviceOption(UserProfile).pipe(
       Effect.andThen((_) => _.pipe(Effect.mapError(() => new NotLoggedInError()))),
@@ -94,6 +72,45 @@ export class UserRepo extends RepositoryDefaultImpl2<UserRepo>()(
 
   static getCurrentUser = Effect.serviceConstants(this).getCurrentUser
   static tryGetCurrentUser = Effect.serviceConstants(this).tryGetCurrentUser
+
+  static readonly getUserByIdResolver = RequestResolver
+    .makeBatched((requests: GetUserById[]) =>
+      this.use((_) =>
+        _
+          .query(Q.where("id", "in", requests.map((_) => _.id)))
+          .pipe(Effect.andThen((users) =>
+            Effect.forEach(requests, (r) =>
+              Request.complete(
+                r,
+                Array
+                  .findFirst(users, (_) => _.id === r.id ? Option.some(Exit.succeed(_)) : Option.none())
+                  .pipe(Option.getOrElse(() => Exit.fail(new NotFoundError({ type: "User", id: r.id }))))
+              ), { discard: true })
+          ))
+      )
+    )
+    .pipe(
+      RequestResolver.batchN(25),
+      RequestResolver.contextFromServices(UserRepo)
+    )
+  static readonly UserFromIdLayer = User
+    .resolver
+    .toLayer(
+      this.use((userRepo) =>
+        this
+          .getUserByIdResolver
+          .pipe(
+            Effect.provideService(this, userRepo),
+            Effect.map((resolver) => ({
+              get: (id: UserId) =>
+                Effect
+                  .request(GetUserById({ id }), resolver)
+                  .pipe(Effect.orDie)
+            }))
+          )
+      )
+    )
+    .pipe(Layer.provide(this.Default))
 }
 
 interface GetUserById extends Request.Request<User, NotFoundError<"User">> {
@@ -101,22 +118,3 @@ interface GetUserById extends Request.Request<User, NotFoundError<"User">> {
   readonly id: UserId
 }
 const GetUserById = Request.tagged<GetUserById>("GetUserById")
-
-const getUserByIdResolver = RequestResolver
-  .makeBatched((requests: GetUserById[]) =>
-    UserRepo
-      .query(Q.where("id", "in", requests.map((_) => _.id)))
-      .pipe(Effect.andThen((users) =>
-        Effect.forEach(requests, (r) =>
-          Request.complete(
-            r,
-            Array
-              .findFirst(users, (_) => _.id === r.id ? Option.some(Exit.succeed(_)) : Option.none())
-              .pipe(Option.getOrElse(() => Exit.fail(new NotFoundError({ type: "User", id: r.id }))))
-          ), { discard: true })
-      ))
-  )
-  .pipe(
-    RequestResolver.batchN(25),
-    RequestResolver.contextFromServices(UserRepo)
-  )
