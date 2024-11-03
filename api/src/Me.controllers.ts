4b177d457cdf63f94d06c7b3b53c80a7aedf0f38
Patrick Roza <contact@patrickroza.com>
2024-11-03 21:20:15 +0100

    refactor: routing5

--- api/src/Me.controllers.ts
+++ api/src/Me.controllers.ts
@@ -3,12 +3,14 @@ import { UserRepo } from "api/services.js"
 import { Effect } from "effect-app"
 import { MeRsc } from "resources.js"
 
-export default matchFor(MeRsc)([
-  UserRepo.Default
-], ({ GetMe }) =>
-  Effect.gen(function*() {
+export default matchFor(MeRsc)({
+  dependencies: [UserRepo.Default],
+  effect: Effect.gen(function*() {
     const userRepo = yield* UserRepo
+
+    const { GetMe } = matchFor(MeRsc)
     return {
       GetMe: GetMe(userRepo.getCurrentUser)
     }
-  }))
+  })
+})
