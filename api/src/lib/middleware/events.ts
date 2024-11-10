03313b31d92ad694551ca7872dad2451e820d44a
Patrick Roza <contact@patrickroza.com>
2024-11-10 18:36:33 +0100

    let's make the scoping part of the consumers instead of the Events service.

--- api/src/lib/middleware/events.ts
+++ api/src/lib/middleware/events.ts
@@ -1,8 +1,9 @@
 import { makeSSE } from "@effect-app/infra/api/middlewares"
 import { Events } from "api/services.js"
-import { Effect, Stream } from "effect-app"
+import { Effect } from "effect-app"
 import { ClientEvents } from "resources.js"
 
-export const makeEvents = Events.pipe(
-  Effect.map((events) => makeSSE(Stream.unwrapScoped(events.stream), ClientEvents))
-)
+export const makeEvents = Effect.gen(function*() {
+  const stream = yield* Events.use((_) => _.stream)
+  return makeSSE(ClientEvents)(stream)
+})
--- api/src/services/Events.ts
+++ api/src/services/Events.ts
@@ -5,11 +5,8 @@ import type { ClientEvents } from "resources.js"
 
 export class Events extends Effect.Service<Events>()("Events", {
   accessors: true,
-  scoped: Effect.gen(function*() {
+  effect: Effect.gen(function*() {
     const q = yield* PubSub.unbounded<{ evt: ClientEvents; namespace: string }>()
-    // we would prefer the http server interrupting the stream processing as part of its shutdown
-    // but that's not happening, and this is the next best thing
-    yield* Effect.addFinalizer(() => q.shutdown)
     const svc = {
       publish: (...evts: NonEmptyReadonlyArray<ClientEvents>) =>
         storeId.pipe(FiberRef.get, Effect.andThen((namespace) => q.offerAll(evts.map((evt) => ({ evt, namespace }))))),
